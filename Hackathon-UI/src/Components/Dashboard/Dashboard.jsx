6f8017640a7fbfcd656fb26dd901e7243d5c0212
sibusiso81 <113394852+Sibusiso81@users.noreply.github.com>
2024-09-01 06:27:58 +0000

    adding form

--- Hackathon-UI/src/Components/Dashboard/Dashboard.jsx
+++ Hackathon-UI/src/Components/Dashboard/Dashboard.jsx
@@ -4,32 +4,38 @@ import { supabase } from "../../superbaseClient";
 import Navbar from "../Navbar/Navbar";
 import myImage from "../Navbar/1.png";
 import { Link, useNavigate } from "react-router-dom";
+import Form from "../Form/Form";
 
 function Dashboard() {
   const [view, setView] = useState("");
   const nevigate = useNavigate();
+
+  
   return (
-    <div className="flex flex-row justify-between font-poppins p-1">
-      <div className="flex flex-row items-center ">
-        <div className="bg-cover">
-          <img
-            className="p-1 rounded-full h-16 w-16 bg-cover"
-            src={myImage}
-            alt="logo"
-          />
+    <section className="w-screen h-screen">
+      <div className="flex flex-row justify-between font-poppins p-1">
+        <div className="flex flex-row items-center ">
+          <div className="bg-cover">
+            <img
+              className="p-1 rounded-full h-16 w-16 bg-cover"
+              src={myImage}
+              alt="logo"
+            />
+          </div>
+          <h1 className="p-1 text-xl font-medium font-poppins">GeoSpartans</h1>
         </div>
-        <h1 className="p-1 text-xl font-medium font-poppins">GeoSpartans</h1>
+        <div className="flex items-center pr-4">
+          <ul className="flex flex-row space-x-4 items-center ">
+            <li className="pr-4">
+              <a href="#home">
+                <Link to={"/chatbot"}>Chatbot</Link>
+              </a>
+            </li>
+          </ul>
+        </div>
       </div>
-      <div className="flex items-center pr-4">
-        <ul className="flex flex-row space-x-4 items-center ">
-          <li className="pr-4">
-            <a href="#home">
-              <Link to={"/chatbot"}>Chatbot</Link>
-            </a>
-          </li>
-        </ul>
-      </div>
-    </div>
+      <Form />
+    </section>
   );
 }
 
--- Hackathon-UI/src/Components/Form/Form.jsx
+++ Hackathon-UI/src/Components/Form/Form.jsx
@@ -1,5 +1,92 @@
-import React from "react";
+import React, { useState } from "react";
 function Form() {
-  return <div>Form</div>;
+  const [cropType, setCropType] = useState("");
+  const [farmSize, setFarmSize] = useState("");
+  const [area, setArea] = useState("");
+  const [season, setSeason] = useState("");
+  const handleSubmit = () => {
+    fetch("https://api/UserInput", {
+      method: "POST", // Specify the request method
+      headers: {
+        "Content-Type": "application/json", // Set the content type header
+      },
+      body: JSON.stringify({
+        cropType: cropType,
+        farmSize: farmSize,
+        Area: area,
+        Season: season,
+      }), // Convert the data to a JSON string
+    })
+      .then((response) => response.json()) // Parse the JSON response
+      .then((data) => console.log(data)) // Handle the response data
+      .catch((error) => console.error("Error:", error)); // Handle any errors
+  };
+  return (
+    /* 
+    Crop type
+    farm size 
+    area
+    season
+    my property
+    */
+    <section className="w-screen h-screen overflow-x-hidden">
+      <div className="grid grid-cols-2 ites-center p-6 mt-10">
+        <div className="col-span-1">
+          <h1 className="p-10 pl-12 text-2xl font-bold font-varaiable mx-auto">
+            Crop Yeild Prediction Form
+          </h1>
+        </div>
+        <div className="col-span-1 flexflex-col">
+          {/* Crop type */}
+          <div class="w-full max-w-sm min-w-[200px] mt-4">
+            <label class="block mb-1 text-sm text-slate-800">Crop Type</label>
+            <input
+              type="text"
+              class="w-full h-10 bg-transparent placeholder:text-slate-400 text-slate-700 text-sm border border-slate-200 rounded px-3 py-2 transition duration-300 ease focus:outline-none focus:border-slate-400 hover:border-slate-400 shadow-sm focus:shadow-md"
+              placeholder=""
+              onChange={(e) => setCropType(e.target.value)}
+            />
+          </div>
+          {/* farm size */}
+          <div class="w-full max-w-sm min-w-[200px] mt-4">
+            <label class="block mb-1 text-sm text-slate-800">Farm Size</label>
+            <input
+              type="text"
+              class="w-full h-10 bg-transparent placeholder:text-slate-400 text-slate-700 text-sm border border-slate-200 rounded px-3 py-2 transition duration-300 ease focus:outline-none focus:border-slate-400 hover:border-slate-400 shadow-sm focus:shadow-md"
+              placeholder="Enter your text"
+              onChange={(e) => setFarmSize(e.target.value)}
+            />
+          </div>
+          {/* area */}
+          <div class="w-full max-w-sm min-w-[200px] mt-4">
+            <label class="block mb-1 text-sm text-slate-800">Area</label>
+            <input
+              type="text"
+              class="w-full h-10 bg-transparent placeholder:text-slate-400 text-slate-700 text-sm border border-slate-200 rounded px-3 py-2 transition duration-300 ease focus:outline-none focus:border-slate-400 hover:border-slate-400 shadow-sm focus:shadow-md"
+              placeholder=""
+              onChange={(e) => setArea(e.target.value)}
+            />
+          </div>
+          {/* Season */}
+          <div class="w-full max-w-sm min-w-[200px] mt-4">
+            <label class="block mb-1 text-sm text-slate-800">Season</label>
+            <input
+              type="text"
+              class="w-full h-10 bg-transparent placeholder:text-slate-400 text-slate-700 text-sm border border-slate-200 rounded px-3 py-2 transition duration-300 ease focus:outline-none focus:border-slate-400 hover:border-slate-400 shadow-sm focus:shadow-md"
+              placeholder="Enter your text"
+              onChange={(e) => setSeason(e.target.value)}
+            />
+          </div>
+          <button
+            className="h-10 w-1/2 mx-auto bg-neutral-950 rounded-md mt-4 p-2 text-white"
+            onClick={handleSubmit}
+          >
+            Submit
+          </button>
+        </div>
+        {/*  */}
+      </div>
+    </section>
+  );
 }
 export default Form;
